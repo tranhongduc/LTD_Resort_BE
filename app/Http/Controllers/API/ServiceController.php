bd308343415f2d73cd185f60581fc50af852094f
Lê Hoàng Long <lelong.ll32@gmail.com>
2023-05-22 10:23:42 +0700

    Paginate API

--- app/Http/Controllers/API/ServiceController.php
+++ app/Http/Controllers/API/ServiceController.php
@@ -77,6 +77,16 @@ class ServiceController extends Controller
         ], 200);
     }
 
+    public function getTotalServices() {
+        $total_services = DB::table('services')->count();
+
+        return response()->json([
+            'message' => 'Query successfully!',
+            'status' => 200,
+            'total_services' => $total_services,
+        ], 200);
+    }
+
     public function getLowestPrice()
     {
         $lowest_price = DB::table('services')->min('price');
@@ -107,5 +117,118 @@ class ServiceController extends Controller
             'status' => 200,
             'list_service_names' => $list_service_names,
         ], 200);
+    }
+
+    public function getTop5LowestPrice()
+    {
+        $list_lowest_price = DB::table('services')->orderBy('price', 'asc')->take(5)->get();
+        
+        return response()->json([
+            'message' => 'Query successfully!',
+            'status' => 200,
+            'list_lowest_price' => $list_lowest_price
+        ]);
+    }
+
+    public function paging(Request $request, $page_number, $num_of_page)
+    {
+        $data = [];
+
+        $list_filter_services = $request->input('list_filter_services');
+
+        if (count($list_filter_services) > 0) {
+            if (count($list_filter_services) % $num_of_page == 0) {
+                $page = count($list_filter_services) / $num_of_page;
+            } else {
+                $page = (int)(count($list_filter_services) / $num_of_page) + 1;
+            }
+
+            $start = ($page_number - 1) * $num_of_page;
+
+            if ($page_number == $page) {
+                for ($i = $start; $i < count($list_filter_services); $i++) {
+                    $data[] = [
+                        "id" => $list_filter_services[$i]['id'],
+                        "service_name" => $list_filter_services[$i]['service_name'],
+                        "image" => $list_filter_services[$i]['image'],
+                        "description" => $list_filter_services[$i]['description'],
+                        "status" => $list_filter_services[$i]['status'],
+                        "price" => $list_filter_services[$i]['price'],
+                        "point_ranking" => $list_filter_services[$i]['point_ranking'],
+                        "service_type_id" => $list_filter_services[$i]['service_type_id'],
+                        "created_at" => $list_filter_services[$i]['created_at'],
+                        "updated_at" => $list_filter_services[$i]['updated_at'],
+                    ];
+                }
+            } else if ($page_number > $page) {
+                $data = [];
+            } else {
+                for ($i = $start; $i < $start + $num_of_page; $i++) {
+                    $data[] = [
+                        "id" => $list_filter_services[$i]['id'],
+                        "service_name" => $list_filter_services[$i]['service_name'],
+                        "image" => $list_filter_services[$i]['image'],
+                        "description" => $list_filter_services[$i]['description'],
+                        "status" => $list_filter_services[$i]['status'],
+                        "price" => $list_filter_services[$i]['price'],
+                        "point_ranking" => $list_filter_services[$i]['point_ranking'],
+                        "service_type_id" => $list_filter_services[$i]['service_type_id'],
+                        "created_at" => $list_filter_services[$i]['created_at'],
+                        "updated_at" => $list_filter_services[$i]['updated_at'],
+                    ];
+                }
+            }
+        } else {
+            $list_services = DB::table('services')->get();
+            if (count($list_services) > 0) {
+                if (count($list_services) % $num_of_page == 0) {
+                    $page = count($list_services) / $num_of_page;
+                } else {
+                    $page = (int)(count($list_services) / $num_of_page) + 1;
+                }
+    
+                $start = ($page_number - 1) * $num_of_page;
+                $data = [];
+    
+                if ($page_number == $page) {
+                    for ($i = $start; $i < count($list_services); $i++) {
+                        $data[] = [
+                            "id" => $list_services[$i]->id,
+                            "service_name" => $list_services[$i]->service_name,
+                            "image" => $list_services[$i]->image,
+                            "description" => $list_services[$i]->description,
+                            "status" => $list_services[$i]->status,
+                            "price" => $list_services[$i]->price,
+                            "point_ranking" => $list_services[$i]->point_ranking,
+                            "service_type_id" => $list_services[$i]->service_type_id,
+                            "created_at" => $list_services[$i]->created_at,
+                            "updated_at" => $list_services[$i]->updated_at,
+                        ];
+                    }
+                } else if ($page_number > $page) {
+                    $data = [];
+                } else {
+                    for ($i = $start; $i < $start + $num_of_page; $i++) {
+                        $data[] = [
+                            "id" => $list_services[$i]->id,
+                            "service_name" => $list_services[$i]->service_name,
+                            "image" => $list_services[$i]->image,
+                            "description" => $list_services[$i]->description,
+                            "status" => $list_services[$i]->status,
+                            "price" => $list_services[$i]->price,
+                            "point_ranking" => $list_services[$i]->point_ranking,
+                            "service_type_id" => $list_services[$i]->service_type_id,
+                            "created_at" => $list_services[$i]->created_at,
+                            "updated_at" => $list_services[$i]->updated_at,
+                        ];
+                    }
+                }
+            }
+        }
+
+        return response()->json([
+            'status' => 200,
+            'list_services' => $data,
+        ]);
     }
 }
--- database/seeders/FeedbackSeeder.php
+++ database/seeders/FeedbackSeeder.php
@@ -14,6 +14,6 @@ class FeedbackSeeder extends Seeder
      */
     public function run(): void
     {
-        Feedback::factory(50)->create();
+        Feedback::factory(1000)->create();
     }
 }
