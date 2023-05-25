d277032072203da8b46fda2d74b55216c632cbb4
lehoanglong12345 <lelong.ll32@gmail.com>
2023-05-25 14:54:16 +0700

    Update API

--- app/Http/Controllers/API/AreaController.php
+++ app/Http/Controllers/API/AreaController.php
@@ -3,45 +3,48 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\room\Area;
 use Illuminate\Support\Facades\DB;
 
-class RoomController extends Controller
+class AreaController extends Controller
 {
-    public function index() {
-        $list_rooms = DB::table('rooms')->get();
+    public function index()
+    {
+        $list_areas = DB::table('areas')->get();
 
         return response()->json([
             'message' => 'Query successfully!',
             'status' => 200,
-            'list_rooms' => $list_rooms,
+            'list_areas' => $list_areas,
         ], 200);
     }
 
-    public function show($id) {
-        $room = DB::table('rooms')->where('id', '=', $id)->first();
+    public function show($id)
+    {
+        $area = DB::table('areas')->where('id', '=', $id)->first();
 
-        if ($room) {
+        if ($area) {
             return response()->json([
                 'message' => 'Query successfully!',
                 'status' => 200,
-                'room' => $room,
+                'area' => $area,
             ], 200);
         } else {
             return response()->json([
                 'message' => 'Data not found!',
                 'status' => 404,
-                'room' => $room,
+                'area' => $area,
             ], 404);
         }
     }
 
-    public function getRoomsByRoomTypeId($id) {
-        $list_rooms = DB::table('rooms')->where('room_type_id', '=', $id)->get();
+    public function getTotalAreas() {
+        $total_areas = DB::table('areas')->count();
 
         return response()->json([
+            'message' => 'Query successfully!',
             'status' => 200,
-            'list_rooms' => $list_rooms,
-        ]);
+            'total_areas' => $total_areas,
+        ], 200);
     }
 }
--- app/Http/Controllers/API/FloorController.php
+++ app/Http/Controllers/API/FloorController.php
@@ -3,45 +3,47 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class RoomController extends Controller
+class FloorController extends Controller
 {
-    public function index() {
-        $list_rooms = DB::table('rooms')->get();
+    public function index()
+    {
+        $list_floors = DB::table('floors')->get();
 
         return response()->json([
             'message' => 'Query successfully!',
             'status' => 200,
-            'list_rooms' => $list_rooms,
+            'list_floors' => $list_floors,
         ], 200);
     }
 
-    public function show($id) {
-        $room = DB::table('rooms')->where('id', '=', $id)->first();
+    public function show($id)
+    {
+        $floor = DB::table('floors')->where('id', '=', $id)->first();
 
-        if ($room) {
+        if ($floor) {
             return response()->json([
                 'message' => 'Query successfully!',
                 'status' => 200,
-                'room' => $room,
+                'floor' => $floor,
             ], 200);
         } else {
             return response()->json([
                 'message' => 'Data not found!',
                 'status' => 404,
-                'room' => $room,
+                'floor' => $floor,
             ], 404);
         }
     }
 
-    public function getRoomsByRoomTypeId($id) {
-        $list_rooms = DB::table('rooms')->where('room_type_id', '=', $id)->get();
+    public function getTotalFloors() {
+        $total_floors = DB::table('floors')->count();
 
         return response()->json([
+            'message' => 'Query successfully!',
             'status' => 200,
-            'list_rooms' => $list_rooms,
-        ]);
+            'total_floors' => $total_floors,
+        ], 200);
     }
 }
